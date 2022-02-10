db1acfa3352728e57721947407848c7a919cf431
MichaelDyvad <michaeldyvad@gmail.com>
2022-02-10 17:34:42 +0100

    done for today

--- src/main/java/kea/sem3/jwtdemo/api/CarController.java
+++ src/main/java/kea/sem3/jwtdemo/api/CarController.java
@@ -6,7 +6,7 @@ import kea.sem3.jwtdemo.service.CarService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//Update
 @RestController
 @RequestMapping("api/cars")
 public class CarController {
